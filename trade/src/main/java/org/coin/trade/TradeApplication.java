1dea1d21b7f12001c028ff880dd70b6de995f626
BA <windy01999@gmail.com>
2023-12-04 22:45:38 +0900

    feat : initial spring project setup

---
+++ trade/src/main/java/org/coin/trade/TradeApplication.java
@@ -0,0 +1,11 @@
+package org.coin.trade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TradeApplication.class, args);
+    }
+}
