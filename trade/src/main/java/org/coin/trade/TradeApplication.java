c362802995903b1e7b9cc74c3f6a8037a9687190
BA <windy01999@gmail.com>
2023-12-11 20:49:27 +0900

    feat: import price module

--- trade/src/main/java/org/coin/trade/TradeApplication.java
+++ trade/src/main/java/org/coin/trade/TradeApplication.java
@@ -3,7 +3,7 @@ package org.coin.trade;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"org.coin.price", "org.coin.trade"})
 public class TradeApplication {
     public static void main(String[] args) {
         SpringApplication.run(TradeApplication.class, args);
